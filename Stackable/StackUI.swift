08c7d88d9fc392263028b040fd8da7c916cf1751
faimin <fuxianchao@gmail.com>
2025-12-01 20:28:59 +0800

    add StackUI

---
+++ Stackable/StackUI.swift
@@ -0,0 +1,64 @@
+//
+//  StackUI.swift
+//  Pods
+//
+//  Created by Zero_D_Saber on 2025/12/1.
+//
+
+import UIKit
+
+public func VStack(
+    distribution: UIStackView.Distribution = .fill,
+    alignment: UIStackView.Alignment = .fill,
+    spacing: CGFloat = 0,
+    @RPStackableBuilder _ stackablesBlock: () -> [any Stackable]
+) -> UIStackView {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.distribution = distribution
+    stackView.alignment = alignment
+    stackView.spacing = spacing
+    
+    stackView.stackable.add(stackablesBlock())
+    
+    return stackView
+}
+
+public func HStack(
+    distribution: UIStackView.Distribution = .fill,
+    alignment: UIStackView.Alignment = .fill,
+    spacing: CGFloat = 0,
+    @RPStackableBuilder _ stackablesBlock: () -> [any Stackable]
+) -> UIStackView {
+    let stackView = UIStackView()
+    stackView.axis = .horizontal
+    stackView.distribution = distribution
+    stackView.alignment = alignment
+    stackView.spacing = spacing
+    
+    stackView.stackable.add(stackablesBlock())
+    
+    return stackView
+}
+
+public func Spacer(
+    minLength: CGFloat? = nil
+) -> UIView {
+    let view = UIView()
+    view.backgroundColor = .clear
+    
+    // Low hugging priority to allow expansion
+    view.setContentHuggingPriority(.defaultLow, for: .horizontal)
+    view.setContentHuggingPriority(.defaultLow, for: .vertical)
+    
+    view.translatesAutoresizingMaskIntoConstraints = false
+    
+    if let minLength {
+        NSLayoutConstraint.activate([
+            view.widthAnchor.constraint(greaterThanOrEqualToConstant: minLength),
+            view.heightAnchor.constraint(greaterThanOrEqualToConstant: minLength),
+        ])
+    }
+    
+    return view
+}
