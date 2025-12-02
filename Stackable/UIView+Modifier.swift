0b29dda7f65d0fa84bb1124009ac8d00d09f4f4a
faimin <fuxianchao@gmail.com>
2025-12-02 13:45:14 +0800

    modify view or UIStackView

---
+++ Stackable/UIView+Modifier.swift
@@ -0,0 +1,62 @@
+//
+//  UIView+Modifier.swift
+//  Pods
+//
+//  Created by Zero.D.Saber on 2025/11/30.
+//
+
+import UIKit
+
+public extension StackableExtension where ExtendedType: UIStackView {
+    @discardableResult
+    func axis(_ axis: NSLayoutConstraint.Axis) -> Self {
+        base.axis = axis
+        return self
+    }
+    
+    @discardableResult
+    func distribution(_ distribution: UIStackView.Distribution) -> Self {
+        base.distribution = distribution
+        return self
+    }
+    
+    @discardableResult
+    func alignment(_ alignment: UIStackView.Alignment) -> Self {
+        base.alignment = alignment
+        return self
+    }
+    
+    @discardableResult
+    func spacing(_ spacing: CGFloat) -> Self {
+        base.spacing = spacing
+        return self
+    }
+}
+
+public extension StackableExtension where ExtendedType: UIView {
+    @discardableResult
+    func width(_ w: CGFloat) -> Self {
+        base.translatesAutoresizingMaskIntoConstraints = false
+        base.widthAnchor.constraint(equalToConstant: w).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    func height(_ w: CGFloat) -> Self {
+        base.translatesAutoresizingMaskIntoConstraints = false
+        base.heightAnchor.constraint(equalToConstant: w).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    func width(_ w: CGFloat) -> ExtendedType {
+        let _: StackableExtension = width(w)
+        return base
+    }
+    
+    @discardableResult
+    func height(_ w: CGFloat) -> ExtendedType {
+        let _: StackableExtension = height(w)
+        return base
+    }
+}
