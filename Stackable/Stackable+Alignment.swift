4bb08b6d988d685e5fd3725b723edda15568dfe6
faimin <fuxianchao@gmail.com>
2025-12-16 18:25:43 +0800

    Fix memory leak

--- Stackable/Stackable+Alignment.swift
+++ Stackable/Stackable+Alignment.swift
@@ -365,20 +365,24 @@ internal extension Stackable {
                 return
             }
             
-            let observation = ancestor.observe(\.frame, options: [.initial, .new]) { _, _ in
+            let observation = ancestor.observe(\.frame, options: [.initial, .new]) { [weak view, weak stackView] _ancestor, _ in
+                guard let view = view,
+                      let stackView = stackView
+                else { return }
+                
                 let bounds = view.bounds
-                let ancestorBounds = view.convert(ancestor.bounds, to: view)
+                let ancestorBounds = view.convert(_ancestor.bounds, to: view)
                 
                 switch stackView.axis {
                 case .horizontal:
-                    let top = (ancestorBounds.minY - bounds.minY) + ancestor.layoutMargins.top
-                    let bottom = (bounds.maxY - ancestorBounds.maxY) + ancestor.layoutMargins.bottom
+                    let top = (ancestorBounds.minY - bounds.minY) + _ancestor.layoutMargins.top
+                    let bottom = (bounds.maxY - ancestorBounds.maxY) + _ancestor.layoutMargins.bottom
                     view.layoutMargins.top = top
                     view.layoutMargins.bottom = bottom
                     
                 case .vertical:
-                    let left = (ancestorBounds.minX - bounds.minX) + ancestor.layoutMargins.left
-                    let right = (bounds.maxX - ancestorBounds.maxX) + ancestor.layoutMargins.right
+                    let left = (ancestorBounds.minX - bounds.minX) + _ancestor.layoutMargins.left
+                    let right = (bounds.maxX - ancestorBounds.maxX) + _ancestor.layoutMargins.right
                     view.layoutMargins.left = left
                     view.layoutMargins.right = right
                     
